7bfd4f8dc61f152381eaffce69bbbf9cdaebe7fd
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-14 20:54:23 -0800

    Auth requests validation created

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'El email es requerido.',
+            'email.email' => 'El campo correo electrónico debe ser una dirección de correo electrónico válida.',
+            'password.required' => 'La contraseña es requerida.',
+        ];
+    }
+}
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:40'],
+            'username' => ['required', 'max:40'],
+            'email' => ['required', 'email', 'unique:users', 'max:40'],
+            'password' => ['required', 'min:6', 'max:30'],
+            'phone' => ['required', 'integer', 'min:6','max:8'],
+            'birthdate' => ['required', 'date'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'El nombre es obligatorio.',
+            'name.max' => 'El nombre no puede tener más de :max caracteres.',
+            'username.required' => 'El usuario es obligatorio.',
+            'username.max' => 'El usuario no puede tener más de :max caracteres.',
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'El correo electrónico debe ser una dirección de correo electrónico válida.',
+            'email.unique' => 'No puedes ocupar este correo electrónico.',
+            'email.max' => 'El correo electrónico no puede tener más de :max caracteres.',
+            'password.required' => 'La contraseña es obligatoria.',
+            'password.min' => 'La contraseña no puede tener menos de :min caracteres.',
+            'password.max' => 'La contraseña no puede tener mas de :max caracteres.',
+            'phone.required' => 'El teléfono es obligatorio.',
+            'phone.integer' => 'El teléfono es invalido',
+            'phone.max' => 'El teléfono no puede tener más de :max dígitos.',
+            'phone.min' => 'El teléfono no puede tener menos de :max dígitos.',
+            'birthdate.required' => 'La fecha de nacimiento es obligatoria.',
+            'birthdate.date' => 'La fecha de nacimiento debe ser una fecha válida.'
+        ];
+    }
+}
