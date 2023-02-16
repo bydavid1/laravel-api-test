6beee1af0f025f3420782e483518e5ac282e0091
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-15 19:32:29 -0800

    Update product request updated

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -24,12 +24,12 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['sometimes', 'string', 'max:255'],
-            'sku' => ['sometimes', 'string', 'max:255', Rule::unique(Product::class, 'sku')->ignore($this->route('id'))],
-            'quantity' => ['sometimes', 'integer', 'min:0'],
-            'price' => ['sometimes', 'numeric', 'min:0'],
-            'description' => ['sometimes', 'string'],
-            'image' => ['sometimes', 'image', 'max:1024'],
+            'name' => ['required', 'string', 'max:255'],
+            'sku' => ['nullable', 'string', 'max:255', Rule::unique(Product::class, 'sku')->ignore($this->route('id'))],
+            'quantity' => ['required', 'integer', 'min:0'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'description' => ['nullable', 'string'],
+            'image' => ['nullable', 'image', 'max:1024'],
         ];
     }
 
@@ -41,13 +41,16 @@ class UpdateProductRequest extends FormRequest
     public function messages()
     {
         return [
+            'name.required' => 'El campo nombre es obligatorio',
             'name.string' => 'El nombre del producto debe ser un texto',
             'name.max' => 'El nombre del producto no debe ser mayor a :max caracteres',
             'sku.string' => 'El SKU del producto debe ser un texto',
             'sku.max' => 'El SKU del producto no debe ser mayor a :max caracteres',
             'sku.unique' => 'El SKU del producto ya está siendo utilizado',
+            'quantity.required' => 'El campo cantidad es obligatorio',
             'quantity.integer' => 'La cantidad debe ser un número entero',
             'quantity.min' => 'La cantidad debe ser un número entero positivo',
+            'price.required' => 'El campo precio es obligatorio',
             'price.numeric' => 'El precio debe ser un número',
             'price.min' => 'El precio debe ser un número positivo',
             'description.string' => 'La descripción del producto debe ser un texto',
