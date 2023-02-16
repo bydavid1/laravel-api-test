aef06473e8a729032e089f865693d9260f25be73
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-16 09:33:26 -0800

    Recovery password mail created

---
+++ resources/views/emails/recovery-password.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Recuperación de contraseña</title>
+</head>
+<body>
+    <h2>Recuperación de contraseña</h2>
+
+    <p>Introduce el siguiente código para restablecer tu contraseña:</p>
+
+    <p><strong>{{ $code }}</strong></p>
+
+    <p>Este código es válido por 10 minutos.</p>
+
+    <p>Si no has solicitado una recuperación de contraseña, ignora este mensaje.</p>
+</body>
+</html>
