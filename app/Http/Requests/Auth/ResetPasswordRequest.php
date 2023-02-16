6ce855221c95f3ab38acd01d67ae0d32fdda783c
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-16 09:51:13 -0800

    Reset password request added messages and validations

--- app/Http/Requests/Auth/ResetPasswordRequest.php
+++ app/Http/Requests/Auth/ResetPasswordRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ResetPasswordRequest extends FormRequest
 {
@@ -22,9 +23,32 @@ class ResetPasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'code' => ['required', 'exists:reset_password_codes,code'],
-            'email' => ['required', 'email', 'exists:users,email'],
+            'email' => ['required', 'email'],
+            'code' => [
+                'required',
+                Rule::exists('reset_password_codes', 'code')->where(function ($query) {
+                    $query->where('email', $this->email);
+                }),
+            ],
             'password' => ['required', 'min:8', 'confirmed'],
+        ];
+    }
+
+        /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages() : array
+    {
+        return [
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.email' => 'Por favor introduce una dirección de correo electrónico válida.',
+            'code.required' => 'El campo código es obligatorio.',
+            'code.exists' => 'El código ingresado no es válido o ha expirado.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.min' => 'La contraseña debe tener al menos :min caracteres.',
+            'password.confirmed' => 'La confirmación de la contraseña no coincide.',
         ];
     }
 }
