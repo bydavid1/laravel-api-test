53732a99923be9cd5fd3f0de399d9dbb404ece7a
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-15 19:32:05 -0800

    Add product with image feature created

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -5,6 +5,7 @@ namespace App\Services\Product;
 use App\Models\Product;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ProductService
 {
@@ -32,8 +33,13 @@ class ProductService
             'name' => $data['name'],
             'quantity' => $data['quantity'],
             'price' => $data['price'],
-            'description' => $data['description']
+            'description' => $data['description'],
         ];
+
+        //If image exists then save
+        if (array_key_exists('image', $data) && $data['image'] != null) {
+            $body['image'] = Storage::putFile('images', $data['image']);
+        }
 
         return Product::create($body);
     }
@@ -47,6 +53,14 @@ class ProductService
             'price' => $data['price'],
             'description' => $data['description']
         ];
+
+        //If image exists then save
+        if (array_key_exists('image', $data) && $data['image'] != null) {
+            //delete last image
+            if ($product->image != null) Storage::delete($product->image);
+
+            $body['image'] = Storage::putFile('images', $data['image']);
+        }
 
         return $product->update($body);
     }
